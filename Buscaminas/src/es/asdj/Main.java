f9a5bd83e4d9f38f84934859a7450749528f3d15
al036991 <al036991@ieslaencanta.com>
2022-04-11 10:31:07 +0200

    buscaminas terminado

--- Buscaminas/src/es/asdj/Main.java
+++ Buscaminas/src/es/asdj/Main.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int lado;
+        int longitud;
+        char[] tablero;
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Bienvenido al buscaminas");
-        lado = pedirNumeroEnRango(entrada, 5, 20,"¿Qué tan largo quieres el tablero? [De 5 a 20] ");
+        longitud = pedirNumeroEnRango(entrada, 5, 20,"¿Qué tan largo quieres el tablero? [De 5 a 20] ");
+        tablero = contruirTablero(longitud);
+        mostrarTablero(tablero);
     }
 
 
@@ -22,7 +25,64 @@ public class Main {
 
         return numero;
     }
-}
-    private static int construirTablero() {
 
+    private static char[] contruirTablero(int tamanyo) {
+        char[] tablero = new char[tamanyo];
+        int minaoNumero;
+
+        for (int i = 0; i < tamanyo; i++) {
+            minaoNumero = (int) (Math.random() * 2);
+            if (minaoNumero == 0) {
+               tablero[i] = '*';
+            } else if (minaoNumero == 1){
+                tablero[i] = '0';
+            }
+        }
+
+        for (int i = 0; i < tamanyo; i++) {
+            if (i != 0 && i != tablero.length - 1 && tablero[i] == '0') {
+                if (tablero[i - 1] == '*' && tablero[i + 1] != '*' || tablero[i + 1] == '*' && tablero[i - 1] != '*') {
+                    tablero[i] = '1';
+                } else if (tablero[i - 1] == '*' && tablero[i + 1] == '*') {
+                    tablero[i] = '2';
+                }
+            } else if (i == 0 && tablero[i] == '0') {
+                if (tablero[i + 1] == '*'){
+                    tablero[i] = '1';
+                }
+            } else if (i == tablero.length - 1) {
+                if (tablero[i - 1] == '*' && tablero[i] == '0'){
+                    tablero[i] = '1';
+                }
+            }
+        }
+
+        return tablero;
     }
+
+    private static void mostrarTablero(char[] tablero) {
+        for (int i = 0; i < tablero.length; i++){
+            if (i < 9) {
+                System.out.print("  " + (i + 1) + " ");
+            } else {
+                System.out.print(" " + (i + 1) + " ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < tablero.length; i++) {
+            System.out.print("+---");
+        }
+        System.out.println("+");
+
+        for (int i = 0; i < tablero.length; i++) {
+            System.out.print("| " + tablero[i] + " ");
+        }
+        System.out.println("|");
+
+        for (int i = 0; i < tablero.length; i++) {
+            System.out.print("+---");
+        }
+        System.out.println("+");
+    }
+}
